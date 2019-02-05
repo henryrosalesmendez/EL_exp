479ef509f567cf1e9428734731a5963cc8a198ba
Henry Rosales Méndez <hrosmendez@gmail.com>
2019-02-04 23:04:44 -0300

    modifications

--- script.py
+++ script.py
@@ -19,9 +19,9 @@ system_t_no_Moscow = "".join(file_system_no_Moscow.readlines())
 file_system_no_Moscow.close()
 
 
-file_system_no_Russian = open("Questionnaire_System_without_Russian.ttl", "r")
-system_t_no_Russian = "".join(file_system_no_Russian.readlines())
-file_system_no_Russian.close()
+file_system_no_heAnd = open("Questionnaire_System_without_he-and-his-four-siblings.ttl", "r")
+system_t_no_heAnd = "".join(file_system_no_heAnd.readlines())
+file_system_no_heAnd.close()
 
 
 
@@ -39,12 +39,12 @@ parser = NIFParser()
 wrp_sys_no_Moscow = parser.parser_turtle(system_t_no_Moscow)
 
 parser = NIFParser()
-wrp_sys_no_Russian = parser.parser_turtle(system_t_no_Russian)
+wrp_sys_no_heAnd = parser.parser_turtle(system_t_no_heAnd)
 
 
 ## --------------------- Measurement
 bmk = NIFBenchmark(wrp_sys, wrp_gold)
-bmk_no_Russian = NIFBenchmark(wrp_sys_no_Russian, wrp_gold)
+bmk_no_heAnd = NIFBenchmark(wrp_sys_no_heAnd, wrp_gold)
 bmk_no_Moscow = NIFBenchmark(wrp_sys_no_Moscow, wrp_gold)
 
 m = {}  # membership function 
@@ -70,20 +70,20 @@ m["https://example.org/doc1#char=131,148"] = 1 # Russian President
 m2 = {}  # membership function 
 m2["https://example.org/doc1#char=21,34"] = 1 # Martin Bashir
 m2["https://example.org/doc1#char=60,87"] = 1 # Living with Michael Jackson
-m2["https://example.org/doc1#char=72,87"] = 0.8 # Michael Jackson
+m2["https://example.org/doc1#char=72,87"] = 1 # Michael Jackson
 m2["https://example.org/doc1#char=93,104"] = 1 # King of Pop
-m2["https://example.org/doc1#char=101,104"] = 0.8 # Pop
+m2["https://example.org/doc1#char=101,104"] = 1 # Pop
 m2["https://example.org/doc1#char=119,122"] = 1 # Joe
-m2["https://example.org/doc1#char=162,164"] = 0.8 # he
-m2["https://example.org/doc1#char=162,186"] = 0.8 # he and his four siblings
-m2["https://example.org/doc1#char=169,172"] = 0.8 # his
-m2["https://example.org/doc1#char=0,7"] = 0.8 # Russian
+m2["https://example.org/doc1#char=162,164"] = 0.3 # he
+m2["https://example.org/doc1#char=162,186"] = 0.3 # he and his four siblings
+m2["https://example.org/doc1#char=169,172"] = 0.3 # his
+m2["https://example.org/doc1#char=0,7"] = 1 # Russian
 m2["https://example.org/doc1#char=14,24"] = 1 # Kommersant
 m2["https://example.org/doc1#char=38,44"] = 1 # Moscow
 m2["https://example.org/doc1#char=61,67"] = 1 # Greeks
-m2["https://example.org/doc1#char=73,76"] = 0.8 # gas
+m2["https://example.org/doc1#char=73,76"] = 0.3 # gas
 m2["https://example.org/doc1#char=102,109"] = 1 # Tsipras
-m2["https://example.org/doc1#char=131,138"] = 0.8 # Russian
+m2["https://example.org/doc1#char=131,138"] = 1 # Russian
 m2["https://example.org/doc1#char=131,148"] = 1 # Russian President
 
 
@@ -97,7 +97,7 @@ print("Micro F1:",bmk_no_Moscow.microF())
 print("Micro FEL:",bmk_no_Moscow.microFEL(m))
 print("Micro FEL_0.8:",bmk_no_Moscow.microFEL(m2))
 
-print("==============================\nExcluding Russian\n-------")
-print("Micro F1:",bmk_no_Russian.microF())
-print("Micro FEL:",bmk_no_Russian.microFEL(m))
-print("Micro FEL_0.8:",bmk_no_Russian.microFEL(m2))
+print("==============================\nExcluding 'he and his four siblings'\n-------")
+print("Micro F1:",bmk_no_heAnd.microF())
+print("Micro FEL:",bmk_no_heAnd.microFEL(m))
+print("Micro FEL_0.8:",bmk_no_heAnd.microFEL(m2))
